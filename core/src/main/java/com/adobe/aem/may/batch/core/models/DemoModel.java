b5af5273f713d1484a3628266f8ada80633f0b34
eoches <itsmailof.krishna@gmail.com>
2025-05-27 15:06:50 +0530

    commit-01

--- core/src/main/java/com/adobe/aem/may/batch/core/models/DemoModel.java
+++ core/src/main/java/com/adobe/aem/may/batch/core/models/DemoModel.java
@@ -1,14 +1,24 @@
 package com.adobe.aem.may.batch.core.models;
 
+import	com.day.cq.wcm.api.Page;
+
+import org.apache.sling.api.resource.ResourceResolver;
+
+import java.beans.ConstructorProperties;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class DemoModel implements Demo {
+@Model(adaptables = {Resource.class,SlingHttpServletRequest.class},  defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class DemoModel{
 
     @ValueMapValue
     private String Title;
@@ -37,6 +47,35 @@ public class DemoModel implements Demo {
     public String getDate() {
         return Date;
     }
+
+    @ScriptVariable
+    Page currentPage;
+
+    public String getArticleTitle()
+    {
+        return currentPage.getPageTitle();
+    }
+
+     public String getArticleTitlePath()
+    {
+        return currentPage.getPath();
+    }
+
+     public Page getArticleTitleParent()
+    {
+        return currentPage.getParent();
+    }
+
+
+ @Inject
+ ResourceResolver resolver;
+
+ public String getNode()
+{
+    return resolver.getUserID();
+}
+
+
 
 
     
