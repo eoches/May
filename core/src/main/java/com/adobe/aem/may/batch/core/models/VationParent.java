9f48ab6af1fae632c8bf272fa06de352d010ad7f
eoches <itsmailof.krishna@gmail.com>
2025-05-29 03:25:06 +0530

    commit-04

--- core/src/main/java/com/adobe/aem/may/batch/core/models/VationParent.java
+++ core/src/main/java/com/adobe/aem/may/batch/core/models/VationParent.java
@@ -1,11 +1,18 @@
 package com.adobe.aem.may.batch.core.models;
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.day.cq.wcm.api.Page;
 
 @Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class VationParent {
@@ -35,6 +42,43 @@ public class VationParent {
 
     public List<VationChild> getBookdetailswithmap() {
         return bookdetailswithmap;
+    }
+
+    @ScriptVariable
+    Page currentPage;
+
+    @ValueMapValue
+    @Named(value = "jcr:primaryType")
+    public String primary;
+
+    public String getArticlePrimaryName()
+    {
+        return primary;
+        
+    }
+
+    @Inject 
+    ResourceResolver resolver;
+
+    public String getArticleTitle()
+    {
+        return currentPage.getTitle();
+    }
+
+    public String getArticlePagePath()
+    {
+        return currentPage.getPath();
+    }
+
+    public Page ArticlePageParent()
+    {
+        return currentPage.getParent();
+    }
+
+    public String getWebContentNode()
+    {
+        return resolver.getUserID();
+        
     }
 
 
